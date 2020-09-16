9af2e79be7159151396a81379d0d6c1285aa4a3e
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-15 23:38:27 -0400

    Initial commit

---
+++ Assets/RLMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RLMovement : MonoBehaviour
+{
+    public Vector3 movePos;
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click");
+            movePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movePos.z = 0;
+        }
+        if (transform.position != movePos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePos, speed * Time.deltaTime);
+        }
+    }
+}
+
+public class ClickPosition : RLMovement
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
