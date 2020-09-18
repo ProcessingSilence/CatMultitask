9d6b46d75debc0a286d0b07ec2b4cd543fc9c791
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-17 21:19:19 -0400

    9:19 PM

--- Assets/Chair.cs
+++ Assets/Chair.cs
@@ -4,44 +4,63 @@ using UnityEngine;
 
 public class Chair : MonoBehaviour
 {
-    public GameObject playerObj;
+    public SpriteRenderer playerSprite;
     private RLMovement _RlMovement_script;
-    private bool inChairFlag;
-    private bool leavingChairFlag;
+    public bool beingClickedOn;
+    public bool playerTouching;
 
     public Sprite[] chairSprites;
-    
+
     private SpriteRenderer _spriteRenderer;
+
+    private CircleCollider2D _circleCollider2D;
+
     // Start is called before the first frame update
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _RlMovement_script = playerObj.GetComponent<RLMovement>();
+        _RlMovement_script = playerSprite.GetComponent<RLMovement>();
+        _circleCollider2D = GetComponent<CircleCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (beingClickedOn && playerTouching)
+        {
+            playerSprite.enabled = false;
+            _spriteRenderer.sprite = chairSprites[1];
+            
+        }
+        else
+        {
+            playerSprite.enabled = true;
+            _spriteRenderer.sprite = chairSprites[0];
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (inChairFlag == false && other.CompareTag("Player"))
+        if (other.CompareTag("Clicker"))
         {
-            inChairFlag = true;
-            PutPlayerInChair();
+            beingClickedOn = true;
         }
+        if (other.CompareTag("Player"))
+        {
+            playerTouching = true;
+        }
+    
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        throw new System.NotImplementedException();
-    }
-
-    void PutPlayerInChair()
-    {
-        playerObj.SetActive(false);
-        _spriteRenderer.sprite = chairSprites[1];
+        if (other.CompareTag("Clicker"))
+        {
+            beingClickedOn = false;
+        }
+        if (other.CompareTag("Player"))
+        {
+            playerTouching = false;
+        }
     }
 }
+
