9109afe8240d1b2efc5f571b33f4be7a10307365
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-18 19:44:53 -0400

    7:44 PM

--- Assets/Scripts/CatAI/Idle.cs
+++ Assets/Scripts/CatAI/Idle.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class Idle : StateMachineBehaviour
 {
-    public Transform catPos;
     public Vector2 randomXRange;
     public Vector2 randomYRange;
     private MyScript _MyScript_script;
     public Movement _Movement_script;
 
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        SetVariables(animator);
+        _MyScript_script.Fire();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        // Set movement position of transform object on Movement.cs
+        _Movement_script.moveToPos.position = _MyScript_script.randomIdleRange;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        Destroy(animator.gameObject.GetComponent<MyScript>());
+    }
+
+    public void SetVariables(Animator animator)
     {
         _Movement_script = animator.gameObject.GetComponent<Movement>();
         _MyScript_script = animator.gameObject.AddComponent<MyScript>();
         _MyScript_script.randomXRange = randomXRange;
         _MyScript_script.randomYRange = randomYRange;
-        
-        _MyScript_script.Fire();
-    }
-
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
-    {
-        _Movement_script.moveToPos.position = _MyScript_script.randomIdleRange;
     }
 
     public class MyScript : MonoBehaviour
@@ -41,7 +50,7 @@ public class Idle : StateMachineBehaviour
                 Random.Range(randomXRange.x, randomXRange.y),
                 Random.Range(randomYRange.x, randomYRange.y));
             
-            yield return new WaitForSecondsRealtime(Random.Range(1f,3f));
+            yield return new WaitForSecondsRealtime(Random.Range(1f,2f));
             StartCoroutine(WaitBeforeMoving());
         }
     }
---
+++ Assets/Scripts/FlipSprite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FlipSprite : MonoBehaviour
+{
+    public Transform movementPos;
+
+    private Vector3 prevPos;
+
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movementPos.position != prevPos)
+        {
+            prevPos = movementPos.position;
+            _spriteRenderer.flipX = transform.position.x < prevPos.x;
+        }
+    }
+}
