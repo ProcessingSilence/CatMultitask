aa3b0310c485352587bc7b9db13d1ac503242e88
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-17 23:43:28 -0400

    11:43 PM

---
+++ Assets/RotateForever.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateForever : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (0,0,rotateSpeed * Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+}
