a1f4da857b5a96c9f55af37817704e4c3b8bd718
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-19 18:44:06 -0400

    6:44 PM

---
+++ Assets/Scripts/TVturnoff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVturnoff : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(TurnOffAnimation());
+    }
+
+    IEnumerator TurnOffAnimation()
+    {
+        _spriteRenderer.color = new Color(255,255,255);
+        yield return new WaitForSecondsRealtime(0.02f);
+        _spriteRenderer.color = new Color(50,50,50);
+        yield return new WaitForSecondsRealtime(0.02f);
+        _spriteRenderer.color = new Color(0,0,0);
+    }
+}
