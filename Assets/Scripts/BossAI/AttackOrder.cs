32ba80834e67077bece42250c20fdc7ba72d2c9f
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-20 11:48:35 -0400

    9/20/2020

---
+++ Assets/Scripts/BossAI/AttackOrder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Array containing boss attack order gets shuffled then iterated through. Reshuffles upon array iteration completion.
+// This is to make it less likely for boss to repeat attacks.
+
+// Iterates when called upon by BossIdle.cs
+public class AttackOrder : MonoBehaviour
+{
+    public bool shuffleOrder;
+    public bool arrayIterate;
+
+    public string[] animatorBools;
+
+    public int iterationNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shuffleOrder)
+        {
+            shuffleOrder = false;
+            FisherYatesShuffle();
+        }
+    }
+
+    // The origin of this was "https://gist.github.com/polysmurf/7393396" in 2019, but the link is dead unfortunately.
+    // So you just have to take my word for this being the source.
+    public void FisherYatesShuffle()
+    {
+        // Loops through array
+        for (int i = animatorBools.Length-1; i > 0; i--)
+        {
+            // Randomize a number between 0 and i (so that the range decreases each time)
+            int rnd = Random.Range(0,i);
+			
+            // Save the value of the current i, otherwise it will overwrite when values swapped.
+            string temp = animatorBools[i];
+			
+            // Swap the new and old values
+            animatorBools[i] = animatorBools[rnd];
+            animatorBools[rnd] = temp;
+        }
+    }
+
+}
--- Assets/Scripts/CatAI/Idle.cs
+++ Assets/Scripts/CatAI/Idle.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Move around in a random range on a random range time iteration
+// Switch state upon random time range, unless infiniteIdle == true.
+
+// Puts temporary monobehavior in gameObject for coroutine purposes until next state.
 public class Idle : StateMachineBehaviour
 {
     public Vector2 randomXRange;
     public Vector2 randomYRange;
+    public Vector2 idleMovementWaitRange;
+    public Vector2 stateTransitionWaitRange;
     
-    private YieldThenNewPos _YieldThenNewPos_script;
+    [HideInInspector] public YieldThenNewPos _YieldThenNewPos_script;
     public Movement _movement_script;
     
     public bool infiniteIdle;
 
+    public string nextBool;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         SetVariables(animator);
@@ -21,12 +28,10 @@ public class Idle : StateMachineBehaviour
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         // Set movement position of transform object on Movement.cs
-        _movement_script.moveToPos.position = _YieldThenNewPos_script.randomIdleRange;
-        if (_YieldThenNewPos_script.moveToWire)
+        _movement_script.moveToPos.position = _YieldThenNewPos_script.randomIdleMovementRange;
+        if (_YieldThenNewPos_script.beginTransition)
         {
-            _YieldThenNewPos_script.moveToWire = false;
-            _YieldThenNewPos_script.infiniteIdle = infiniteIdle;
-            animator.SetBool("movingToWire", true);
+            animator.SetBool(nextBool, true);
             animator.SetBool("idle", false);
             Debug.Log("Moving to wire");
         }
@@ -37,48 +42,59 @@ public class Idle : StateMachineBehaviour
         Destroy(animator.gameObject.GetComponent<YieldThenNewPos>());
     }
 
+    // I probably could have made this more organized had I used static variables, but lack of time says it's too late.
+    // This looks so ugly.
     public void SetVariables(Animator animator)
     {
         _movement_script = animator.gameObject.GetComponent<Movement>();
         if (_movement_script.enabled == false)
         {
             _movement_script.enabled = true;
-        }
-        
+        }       
         _YieldThenNewPos_script = animator.gameObject.AddComponent<YieldThenNewPos>();
         _YieldThenNewPos_script.randomXRange = randomXRange;
         _YieldThenNewPos_script.randomYRange = randomYRange;
+        _YieldThenNewPos_script.beginTransition = false;
+        _YieldThenNewPos_script.stateTransitionWaitRange = stateTransitionWaitRange;
+        _YieldThenNewPos_script.idleMovementWaitRange = idleMovementWaitRange;
+        _YieldThenNewPos_script.infiniteIdle = infiniteIdle;
     }
 
     public class YieldThenNewPos : MonoBehaviour
     {
         public Vector2 randomXRange;
         public Vector2 randomYRange;
-        public Vector2 randomIdleRange;
+        public Vector2 randomIdleMovementRange;
+        public Vector2 stateTransitionWaitRange;
+        public Vector2 idleMovementWaitRange;
 
-        public bool moveToWire;
+        public bool beginTransition;
         public bool infiniteIdle;
+        
         public void Fire()
         {
             StartCoroutine(WaitBeforeMoving());
+            
             if (infiniteIdle == false)
-                StartCoroutine(WaitBeforeMovingToWire());
+            {
+                StartCoroutine(WaitBeforeStateTransition());
+            }
         }
 
         IEnumerator WaitBeforeMoving()
         {        
-            randomIdleRange = new Vector2(
+            randomIdleMovementRange = new Vector2(
                 Random.Range(randomXRange.x, randomXRange.y),
                 Random.Range(randomYRange.x, randomYRange.y));
             
-            yield return new WaitForSecondsRealtime(Random.Range(1f,2f));
+            yield return new WaitForSecondsRealtime(Random.Range(idleMovementWaitRange.x,idleMovementWaitRange.y));
             StartCoroutine(WaitBeforeMoving());
         }
 
-        IEnumerator WaitBeforeMovingToWire()
+        IEnumerator WaitBeforeStateTransition()
         {
-            yield return new WaitForSecondsRealtime(Random.Range(1f,10f));
-            moveToWire = true;
+            yield return new WaitForSecondsRealtime(Random.Range(stateTransitionWaitRange.x,stateTransitionWaitRange.y));
+            beginTransition = true;
         }
     }
 }
